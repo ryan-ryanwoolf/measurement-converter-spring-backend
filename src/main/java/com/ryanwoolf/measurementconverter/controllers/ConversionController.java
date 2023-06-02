29d386d29fba323046d55d1261e954c6deea1d75
RyanWoolfTechnicalAssessment <124464786+RyanWoolfTechnicalAssessment@users.noreply.github.com>
2023-06-02 22:03:07 +0200

    trigger second attempt

--- src/main/java/com/ryanwoolf/measurementconverter/controllers/ConversionController.java
+++ src/main/java/com/ryanwoolf/measurementconverter/controllers/ConversionController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class ConversionController {
 
-    
+
     @Autowired
     TemperatureService temperatureService;
 
